5671c2d3b177cab1a46f1bb67f180c84a6ffe819
Alexandr Sivakov <alexandrsivakov@me.com>
2015-12-04 23:32:54 +0300

    Not needed commit

--- CourseProject/CourseProject.cpp
+++ CourseProject/CourseProject.cpp
@@ -133,6 +133,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             {
             case IDM_ABOUT:
                 DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+				;
                 break;
             case IDM_EXIT:
                 DestroyWindow(hWnd);
